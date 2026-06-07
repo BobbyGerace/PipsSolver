9b4fe08d8a54dd4b038ed0a9cf9a147eecd46853
Bobby Gerace <rfgerace@gmail.com>
2026-06-07 10:05:22 -0400

    Added perf check

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using PipsSolver.IO;
+using PipsSolver.Model;
 
 namespace PipsSolver;
 
@@ -7,21 +8,70 @@ class Program
 {
   public static void Main(string[] args)
   {
-    var (board, constraints, dominos) = new FileParser(args[0]).Parse();
+    if (args[0] == "--perf") Perf();
+    else SolveFile(args[0]);
+  }
 
-    var watch = Stopwatch.StartNew();
-    var solved = new Solver(board, dominos).Solve();
-    watch.Stop();
-
-    if (solved)
-    {
-      Console.WriteLine($"Solved in {watch.ElapsedMilliseconds} ms");
-      new FancyPrinter(board).Print();
-    }
-    else
+  public static void SolveFile(string filePath)
+  {
+    if (ParseAndSolve(filePath) is not (Board board, double ms))
     {
       Console.WriteLine("Game is not solvable");
+      return;
     }
+
+    Console.WriteLine($"Solved in {ms} ms");
+    new FancyPrinter(board).Print();
+  }
+
+  public static void Perf()
+  {
+    var files = Directory.EnumerateFiles("./examples");
+
+    // Run once and throw away to warm up JIT
+    ParseAndSolve(files.First());
+
+    List<double> easys = [];
+    List<double> mediums = [];
+    List<double> hards = [];
+
+    foreach (string filePath in files)
+    {
+      if (ParseAndSolve(filePath) is not (_, double ms))
+      {
+        throw new Exception("Unsolvable game in examples folder");
+      }
+
+      Console.WriteLine($"{filePath}: {ms}");
+
+      if (filePath.Contains("easy")) easys.Add(ms);
+      else if (filePath.Contains("medium")) mediums.Add(ms);
+      else if (filePath.Contains("hard")) hards.Add(ms);
+    }
+
+    Console.WriteLine("");
+    Console.WriteLine("---");
+    Console.WriteLine("");
+
+    Console.WriteLine($"Easy avg: {easys.Average()}");
+    Console.WriteLine($"Medium avg: {mediums.Average()}");
+    Console.WriteLine($"Hard avg: {hards.Average()}");
+
+    var all = easys.Concat(mediums).Concat(hards);
+
+    Console.WriteLine($"Overall avg: {all.Average()}");
+    Console.WriteLine($"Max: {all.Max()}");
+  }
+
+  private static (Board board, double ms)? ParseAndSolve(string filePath)
+  {
+      var (board, constraints, dominos) = new FileParser(filePath).Parse();
+
+      var watch = Stopwatch.StartNew();
+      var solved = new Solver(board, dominos).Solve();
+      watch.Stop();
+
+      return (board, watch.Elapsed.TotalMilliseconds);
   }
 }
 
